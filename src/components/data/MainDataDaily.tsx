020317d3bd44499faf62efa294cee789e6f40c8d
vickbk <vickbake8@gmail.com>
2025-09-18 20:09:42 +0200

    daily forcast styling

--- src/components/data/MainDataDaily.tsx
+++ src/components/data/MainDataDaily.tsx
@@ -3,9 +3,9 @@ import iconRain from "@images/icon-rain.webp";
 
 export default function MainDataDaily() {
   return (
-    <section className="main__data-daily">
-      <h4 className="daily-heading">Daily Forecast</h4>
-      <div className="daily-days">
+    <section className="main__data-daily mbl-1">
+      <h4 className="daily-heading mb-1">Daily Forecast</h4>
+      <div className="daily-days grid gc-3 g-1">
         {[
           {
             day: "Tue",
@@ -25,12 +25,15 @@ export default function MainDataDaily() {
             highest: string;
             lowest: string;
           }) => (
-            <article key={day} className="days-day">
+            <article
+              key={day}
+              className="days-day text-center neutral-700 p-1 br-1"
+            >
               <h5 className="day-title">{day}</h5>
-              <Image src={image} alt={desc} />
-              <p className="day-footer">
-                <span>{highest}</span>
-                <span>{lowest}</span>
+              <Image src={image} alt={desc} height={60} className="mbl-1" />
+              <p className="day-footer flex space-between">
+                <span>{highest}°</span>
+                <span>{lowest}°</span>
               </p>
             </article>
           )
