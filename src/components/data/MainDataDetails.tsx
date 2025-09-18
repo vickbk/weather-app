41b4b1dc85a46b3a4a7a511d7489527b2d270265
vickbk <vickbake8@gmail.com>
2025-09-18 19:31:16 +0200

    details initial styling

--- src/components/data/MainDataDetails.tsx
+++ src/components/data/MainDataDetails.tsx
@@ -1,15 +1,15 @@
 export default function MainDataDetails() {
   return (
-    <section className="data__details">
+    <section className="data__details grid gc-2 g-1">
       {[
         ["Feels Like", "18"],
         ["Humidity", "46%"],
         ["Wind", "14km/h"],
         ["Precipitation", "0 mm"],
       ].map(([title, content]: string[]) => (
-        <article className="details-element" key={title}>
+        <article className="details-element neutral-700 p-1 br-1" key={title}>
           <h3 className="details-element-title">{title}</h3>
-          <p className="details-element-content">{content}</p>
+          <p className="details-element-content smt-5">{content}</p>
         </article>
       ))}
     </section>
