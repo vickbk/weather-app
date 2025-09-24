33e54431c441c6cb5542911cdc2f5d9ae7288935
vickbk <vickbake8@gmail.com>
2025-09-24 04:59:33 +0200

    Fixing height of the hourly section to align on data fetch

--- src/components/data/HourlyData.tsx
+++ src/components/data/HourlyData.tsx
@@ -4,6 +4,7 @@ import { LoadingStatus } from "@/lib/types/loading-status";
 import { Skeleton } from "@progress/kendo-react-indicators";
 import { WeatherHourlyData } from "@/lib/types/weather-data";
 import weatherIcons from "../common/WeatherIcons";
+import { useEffect, useRef, useState } from "react";
 
 export default function HourlyData({
   status,
@@ -25,31 +26,46 @@ export default function HourlyData({
             temp: `${temp?.toFixed()}`,
             icon: weatherIcons.get(weatherCode!, time),
           })) ?? [];
+  const articleRef = useRef<HTMLElement>(null);
+  const headerRef = useRef<HTMLElement>(null);
+  const holderRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    setTimeout(() => {
+      if (articleRef.current && headerRef.current && holderRef.current) {
+        const holder = holderRef.current;
+        holder.style.maxBlockSize = "600px";
+        holder.style.maxBlockSize = `calc(${
+          articleRef.current.offsetHeight - headerRef.current.offsetHeight
+        }px - 3em)`;
+      }
+    }, 300);
+  }, [hourly]);
 
   return (
-    <article className="data__hourly neutral-700 br-1 p-1">
-      <section className="hourly">
-        <div className="flex space-between center">
-          <h4 className="hourly__title">Hourly forecast</h4>
-          <Dropdown
-            specialClass="neutral-600"
-            text={status !== "loading" ? "The day" : " - "}
-          />
-        </div>
-        <div className="hourly__data-holder grid mbls-1 g-1">
-          {data.map(({ temp, time, icon }, index) =>
-            status === "loading" ? (
-              <Skeleton
-                key={index}
-                shape="rectangle"
-                className="hourly__loader p-2 br-1"
-                animation={{ type: "wave" }}
-              />
-            ) : (
-              <DataPerHour key={index} temp={temp} time={time} icon={icon} />
-            )
-          )}
-        </div>
+    <article
+      ref={articleRef}
+      className="data__hourly hourly neutral-700 br-1 p-1"
+    >
+      <section ref={headerRef} className="flex space-between center">
+        <h4 className="hourly__title">Hourly forecast</h4>
+        <Dropdown
+          specialClass="neutral-600"
+          text={status !== "loading" ? "The day" : " - "}
+        />
+      </section>
+      <section ref={holderRef} className="hourly__data-holder grid mbls-1 g-1">
+        {data.map(({ temp, time, icon }, index) =>
+          status === "loading" ? (
+            <Skeleton
+              key={index}
+              shape="rectangle"
+              className="hourly__loader p-2 br-1"
+              animation={{ type: "wave" }}
+            />
+          ) : (
+            <DataPerHour key={index} temp={temp} time={time} icon={icon} />
+          )
+        )}
       </section>
     </article>
   );
