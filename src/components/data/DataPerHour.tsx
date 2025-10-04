8a3429f98be59b83561ff2775030dc1182da5041
vickbk <vickbake8@gmail.com>
2025-10-04 20:31:11 +0200

    Initiating the details for more info on hourly data

--- src/components/data/DataPerHour.tsx
+++ src/components/data/DataPerHour.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import dropdown from "@images/icon-dropdown.svg";
+import HourlyDetails from "./hourly/HourlyDetails";
+import { useState } from "react";
 
 export default function DataPerHour({
   icon: { image, desc },
@@ -9,11 +12,20 @@ export default function DataPerHour({
   time: string;
   temp: string;
 }) {
+  const [details, showDetails] = useState(false);
+  const [arrow, showArrow] = useState(false);
   return (
-    <section className="hourly__data sp-5 pi-1 flex sg-5 sm-up-g-2 lg-up-sg-5 center br-1 neutral-600">
+    <article
+      className="hourly__data sp-5 pi-1 flex flex-wrap sg-5 sm-up-g-2 lg-up-sg-5 center br-1 neutral-600 cursor-p"
+      onClick={() => showDetails(!details)}
+      onMouseEnter={() => showArrow(true)}
+      onMouseLeave={() => showArrow(false)}
+    >
       <Image src={image} alt={desc} height={40} />
       <p className="hourly__data-time">{time}</p>
       <p className="mis-auto">{temp}°</p>
-    </section>
+      {arrow && <Image src={dropdown} alt="" />}
+      {details && <HourlyDetails />}
+    </article>
   );
 }
---
+++ src/components/data/hourly/HourlyDetails.tsx
@@ -0,0 +1,10 @@
+import DetailElement from "../data-details/DetailElement";
+
+export default function HourlyDetails() {
+  return (
+    <section className="flex-grow grid sg-5 gc-2">
+      <DetailElement title="any" content="any" />
+      <DetailElement title="any" content="any" />
+    </section>
+  );
+}
