702f791a87ef8aee1bf2509fbb6c63b119342c14
vickbk <vickbake8@gmail.com>
2025-10-06 12:03:10 +0200

    Improving accessibility for DatePerHour element by changing article element with a button

--- src/components/data/DataPerHour.tsx
+++ src/components/data/DataPerHour.tsx
@@ -4,6 +4,14 @@ import HourlyDetails from "./hourly/HourlyDetails";
 import { useState } from "react";
 import { WeatherHourlyData } from "@/lib/types/weather-data";
 import { getDataForDisplay } from "@/lib/open-meteo/process-hourlydata";
+import { SvgIcon } from "@progress/kendo-react-common";
+import {
+  arrowDownIcon,
+  arrowUpIcon,
+  closedCaptionsIcon,
+  crosstabIcon,
+  dropdownIcon,
+} from "@progress/kendo-svg-icons";
 
 export default function DataPerHour({ hourly }: { hourly: WeatherHourlyData }) {
   const {
@@ -13,15 +21,20 @@ export default function DataPerHour({ hourly }: { hourly: WeatherHourlyData }) {
   } = getDataForDisplay(hourly);
   const [details, showDetails] = useState(false);
   return (
-    <article
-      className="hourly__data sp-5 pi-1 flex flex-wrap sg-5 sm-up-g-2 lg-up-sg-5 center br-1 neutral-600 cursor-p"
-      onClick={() => showDetails(!details)}
-    >
-      <Image src={image} alt={desc} height={40} />
-      <p className="hourly__data-time">{time}</p>
-      <p className="mis-auto">{temp}°</p>
-      <Image src={dropdown} alt="" className="details-dropdown" />
+    <>
+      <button
+        className="hourly__data no-border sp-5 pi-1 flex flex-wrap sg-5 sm-up-g-2 lg-up-sg-5 center br-1 neutral-600 cursor-p"
+        onClick={() => showDetails(!details)}
+      >
+        <Image src={image} alt={desc} height={40} />
+        <p className="hourly__data-time">{time}</p>
+        <p className="mis-auto">{temp}°</p>
+        <SvgIcon
+          icon={details ? arrowUpIcon : dropdownIcon}
+          className="details-dropdown"
+        />
+      </button>
       {details && <HourlyDetails data={hourly} />}
-    </article>
+    </>
   );
 }
