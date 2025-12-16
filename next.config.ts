5c5f5e6b45a0958437ad44f966ad1e111a78409e
vickbk <vickbake8@gmail.com>
2025-12-16 14:23:05 +0200

    adding turbo support

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import withSerwistInit from "@serwist/next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   async headers() {
     return [
       {
